6d08a98e92a46e411004a544f8239a0841320028
Chad McKee <chadm_@yahoo.com>
2017-06-24 21:41:47 -0400

    contact form validation

---
+++ js/contact-script.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mephisto on 6/24/17.
+ */
+
+$(document).ready(function () {
+    $('#contact-form').submit(function(event){
+        event.preventDefault();
+        console.log('form submitted');
+        $('input').each(function(){
+            // var currentInputTagClass = $(this).attr('class');
+            // console.log(currentInputTagClass);
+            // var errorDivClassName = '.' + currentInputTagClass + '-error';
+            // console.log(errorDivClassName);
+            if($(this).val() ==''){
+                $('#form-error').html('*All input fields are required.')
+            }
+        });
+
+
+    });
+});
